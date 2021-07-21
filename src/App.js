8dbe65ac74aae1f0ced24af9c68ed5fac9375783
Ellezique <gizellevanzyl@gmail.com>
2021-07-21 13:23:44 +1000

    Add rfce boilerplate to pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+//import './App.css'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Contact from '.components/pages/Login'
+import Contact from '.components/pages/SignUp'
+
 
 const App = () => {
   return (
