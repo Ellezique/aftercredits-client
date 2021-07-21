c745996ae496fff9817f343fa92f7ac1421ea44b
Ellezique <gizellevanzyl@gmail.com>
2021-07-21 15:30:22 +1000

    Add Navbar logo linking to home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,25 +6,30 @@ function Navbar() {
     <>
       <nav className="navbar">
         <div className="navbar-container">
+          {/* Navbar Logo and AfterCredits  */}
+            <Link to='/' className="logo-section">
+              <i class="fas fa-film"> AfterCredits </i>
+            </Link>
+          {/* Navbar Links: */}
           <ul>
             <li className='nav-item'>
               <Link Link to='/' className='nav-links'>
-                Home
+                HOME
               </Link>
             </li>
             <li className='nav-item'>
               <Link Link to='/LogIn' className='nav-links'>
-                LogIn
+                LOG IN
               </Link>
             </li>
             <li className='nav-item'>
               <Link Link to='/SignUp' className='nav-links'>
-                SignUp
+                SIGN UP
               </Link>
             </li>
             <li className='nav-item'>
               <Link to='/Contact' className='nav-links'>
-                Contact
+                CONTACT
               </Link>
             </li>
           </ul>
