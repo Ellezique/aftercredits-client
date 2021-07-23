f6125465c00d2c74d9bf40a5859ba7b9333db167
chris-gibs <theguyfromdubai@hotmail.com>
2021-07-23 16:35:32 +1000

    Created basic Api component in utils folder, added basic post request functionality to home page, will need some editing for the cards and will need to add a function for each kind of request

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
 function Home() {
+
+  //calls api component to fetch ids from our database
+  const getIds = async () => {
+    try {
+      //Destructures data from response
+      const { data } = await api.post('', {
+        
+      })
+      console.log(data)
+        // localStorage.setItem('jwt', data.jwt)
+        
+    } catch (error) {
+      console.log("Error: " + error.message)
+    }
+  }
+
+
   return (
     <div>
+      {getIds()}
       <h1>HOME PAGE</h1>
     </div>
   )
---
+++ src/utils/Api.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+let apiUrl = ''
+
+if(database) {
+  apiUrl = ''
+}
+if(movie) {
+  apiUrl = 'https://movie-database-imdb-alternative.p.rapidapi.com/'
+}
+if(google) {
+  apiUrl = ''
+}
+
+export const api = axios.create({
+  baseURL: apiUrl
+})
+
+api.interceptors.request.use((req) => {
+  const jwt = localStorage.getItem('jwt')
+  if (jwt) {
+    req.headers["Authorization"] = `Bearer ${jwt}`
+  }
+  return req
+})
