ebdd7cdfdda62a4186bbee20c3b5230569559d4b
Ellezique <gizellevanzyl@gmail.com>
2021-07-28 17:50:21 +1000

    Bing maps - add zoom, disable panning and mouse button scroll

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,21 +6,25 @@ import BingMapsReact from "bingmaps-react";
 console.log (process.env) 
 
 function Contact() {
-  // const API_KEY = process.env.REACT_APP_BING_API_KEY;
+ 
   return (
     <div>
       <h1>CONTACT PAGE</h1>
       <h1>Address goes here</h1>
+      {/* Bing Maps for react */}
       <BingMapsReact
         bingMapsKey = {process.env.REACT_APP_BING_API_KEY}
         height="500px"
         mapOptions={{
           navigationBarMode: "square",
+          disableScrollWheelZoom: true,
+          disablePanning: true,
         }}
         max-width="100%"
         viewOptions={{
           center: { latitude: -27.468150020880756, longitude: 153.02513165952453 },
           mapTypeId: "road",
+          zoom: 15,
         }}
       />
     </div>
