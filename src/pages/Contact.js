c7ef2b51f1177bebabfd5db9976d60a3d539fe93
Ellezique <gizellevanzyl@gmail.com>
2021-07-28 17:21:02 +1000

    Add bingmaps with maptype road and address latitude and longitude, apikey in env file that is ignored by git

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import BingMapsReact from "bingmaps-react";
+// import './Contact.css'
+// require('dotenv').config()
 
-export default function Contact() {
+console.log (process.env) 
+
+function Contact() {
+  // const API_KEY = process.env.REACT_APP_BING_API_KEY;
   return (
     <div>
       <h1>CONTACT PAGE</h1>
+      <h1>Address goes here</h1>
+      <BingMapsReact
+        bingMapsKey = {process.env.REACT_APP_BING_API_KEY}
+        height="500px"
+        mapOptions={{
+          navigationBarMode: "square",
+        }}
+        max-width="100%"
+        viewOptions={{
+          center: { latitude: -27.468150020880756, longitude: 153.02513165952453 },
+          mapTypeId: "road",
+        }}
+      />
     </div>
-  )
+  );
 }
+
+export default Contact
