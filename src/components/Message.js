ab9e9bb690195b5d11bab902e95ddd89ec5b591e
chris-gibs <theguyfromdubai@hotmail.com>
2021-07-30 13:56:25 +1000

    Added message options for edit (Which currently works) and delete (which currently console logs), both will need fetch requests to the server for update/delete functionality, will need to consider if a rerender of messages should occur when one is deleted which may require a fetch from the server after a delete request has been sent to get the updated message list

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,20 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 import './Message.css'
 
 
 export default function Message({message}) {
   const {username, text, date} = message
+  const [editing, setEditing] = useState(false)
+  const [options, setOptions] = useState(false)
+  const [messageText, setMessageText] = useState(text)
+
+
+  function handleChange(event){
+    setMessageText(event.target.value)
+  }
+
   return (
     <div className='MessageContainer'>
-      <p className='MessageUser'>{username}</p>
-      <p className='MessageText'>{text}</p>
-      <p className='MessageDate'>{date}</p>
-      <Button 
-        text='Edit'
-        //callback={}  implement edit functionality
-        // icon='' grab later
-      />
+      {!editing ?
+        <>
+          <p className='MessageUser'>{username}</p>
+          <p className='MessageText'>{messageText}</p>
+          <p className='MessageDate'>{date}</p>
+          <Button 
+            text='Options'
+            callback={() => {
+              setOptions(!options)
+            }}
+          />
+          {options &&
+            <div className='OptionsContainer'>
+              <Button 
+                text='Edit'
+                callback={() => {
+                  setOptions(!options)
+                  setEditing(true)
+                  console.log('edit')
+                }}
+              />
+              <Button 
+                text='Delete'
+                callback={() => {
+                  setOptions(!options)
+                  console.log('delete')
+                }}
+              />           
+            </div>
+          }
+        </>
+      :
+        <>
+          <textarea value={messageText} onChange={handleChange} />
+          <Button 
+            text='Save Changes'
+            callback={() => {
+              // PUT REQUEST TO MESSAGE TABLE
+
+              setEditing(false)
+              console.log('save')
+            }}
+            // icon='' grab later
+          />
+        </>
+      }
     </div>
   )
 }
