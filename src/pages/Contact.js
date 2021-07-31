2334839bc8922daf83c69f7b0a9019db720eced1
Ellezique <gizellevanzyl@gmail.com>
2021-07-31 16:38:23 +1000

    Styling: Box shadow and radius on maps and address & neon lights on navbar

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,35 +7,36 @@ function Contact() {
   return (
     <div>
       <h1>CONTACT PAGE</h1>
-      <h3 class="address">AfterCredits Head Office</h3>
-      <p class="address">Level 3/116 Adelaide St, Brisbane City QLD 4000</p>
-      <p class="address">Brisbane City</p>
-      <p class="address">QLD 4000</p>
-      <BingMapsReact
-        bingMapsKey = {process.env.REACT_APP_BING_API_KEY}
-        height="500px"
-        mapOptions={{
-          navigationBarMode: "square",
-          disableScrollWheelZoom: true,
-          // disablePanning: true,
-          showBreadcrumb: true, //The breadcrumb control shows the current center location’s geography hierarchy. Australia > QLD > Brisbane City
-          showLocateMeButton: true,
-          showScalebar: true, //A boolean value indicating whether to show the scale bar. 
-          showZoomButtons: true, //how the zoom buttons in the map navigation control.
-        }}
-      
-        max-width="100%"
-        viewOptions={{
-          center: center,
-          mapTypeId: "road", //default loads road map. arial and bird's eye view are still selectable options
-          zoom: 15, 
-        }} 
-        
-        pushPinsWithInfoboxes ={[
-          {center, metadata: {title: "AfterCredits", description: "Level 3/116 Adelaide St, <br> Brisbane City <br> QLD 4000"}}
-      ]}
-       
-     ></BingMapsReact>
+      <div className = "addressbox">
+        <h3>AfterCredits</h3>
+        <p>Level 3/116 Adelaide St,</p>
+        <p>Brisbane City</p>
+        <p >QLD 4000</p>
+      </div>
+      <div className="bingmap">
+        <BingMapsReact
+          bingMapsKey = {process.env.REACT_APP_BING_API_KEY}
+          height="500px"
+          mapOptions={{
+            navigationBarMode: "square",
+            disableScrollWheelZoom: true, 
+            // disablePanning: true,
+            showBreadcrumb: true, //The breadcrumb control shows the current center location’s geography hierarchy. Australia > QLD > Brisbane City
+            showLocateMeButton: true,
+            showScalebar: true, //A boolean value indicating whether to show the scale bar. 
+            showZoomButtons: true, //how the zoom buttons in the map navigation control.
+          }}
+          max-width="100%"
+          viewOptions={{
+            center: center,
+            mapTypeId: "road", //default loads road map. arial and bird's eye view are still selectable options
+            zoom: 15, 
+          }}   
+          pushPinsWithInfoboxes ={[
+            {center, metadata: {title: "AfterCredits", description: "Level 3/116 Adelaide St, <br> Brisbane City <br> QLD 4000"}}
+        ]}
+        ></BingMapsReact>
+      </div>
     </div>
   );
 }
