fb667465adac88e717833a1a080db2be6fd3c93b
Ellezique <gizellevanzyl@gmail.com>
2021-08-04 23:23:50 +1000

    Delete services folder and comment out acios in api.js file as unable to deploy netlify

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-const aftercreditsAPI = axios.create({
-  baseURL: "http://localhost:3000"
-})
+// const aftercreditsAPI = axios.create({
+//   baseURL: "http://localhost:3000"
+// })
 
-export default aftercreditsAPI
+// export default aftercreditsAPI
