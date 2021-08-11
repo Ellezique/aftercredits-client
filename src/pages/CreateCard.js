0a113332deabacaf064232e3a4a11dad5fcfca8b
Ellezique <gizellevanzyl@gmail.com>
2021-08-11 14:14:53 +1000

    Add styling and more UX to create new card page

--- src/pages/CreateCard.js
+++ src/pages/CreateCard.js
@@ -7,29 +7,43 @@ export default function CreateCard() {
   const [cardId, setCardId] = useState('')
 
   return (
-    <div className='createCardContainer'>
-      <h1>New Movie/Series Card</h1>
-      <p>Go to <a href='https://www.imdb.com/'>IMDB</a> and search for the movie/series you are looking for using IMDB's search tool shown below.</p>
-      <img src='./../../images/IMDB.png' alt='IMDB Page'/>
-      <p>When on your chosen movie/series page, look at the url at the top of the browser and extract the id (Starts with tt) from the url.</p>
-      <img src='./../../images/tt_id.png' alt='Url Link'/>
-      <p>Enter the tt id below:</p>
-      <textarea
-          value={cardId}
-          onChange={(event) => {
-            setCardId(event.target.value)
-          }}
-        />
-      <Link to='/' >
-      <Button
-        text='Create New Card'
-        callback={() => {
-          // ADD CARD TO BACKEND
-          Api.serverApi.cards.create({imdb_id: cardId})
-          console.log('new card, what do you think?')
-        }}
-      />
-      </Link>
-    </div>
+    <>
+      <video src='https://res.cloudinary.com/ellezique/video/upload/v1628311932/smokeloop_oubxna.mp4' autoPlay loop muted />
+      <div className='gencontainer'>
+        <h1>New Movie/Series Card</h1>
+        <h3>Go to <a href='https://www.imdb.com/' className="hyperlink" target="_blank" rel="noreferrer">Internet Movie Database</a> and search for the movie/series you are looking for using IMDB's search tool shown below:</h3>
+        <a href='https://www.imdb.com/' target="_blank" rel="noreferrer">
+          <img src='./../../images/imdbsearch.PNG' alt='IMDB Page'/>
+        </a>
+        <br></br>
+        <div className="imdbcontainer">
+          <h3>When on your chosen movie/series page, look at the url at the top of the browser and extract the id (Starts with tt) from the url.</h3>
+          <a href='https://www.imdb.com/' target="_blank" rel="noreferrer">
+            <img src='./../../images/ttid.PNG' alt='Url Link Showing id'/>
+          </a>
+        </div>
+        <br></br>
+        <div className="imdbcontainer">
+          <p>Enter the tt id below:</p>
+          <textarea
+            placeholder="tt0111161"
+            value={cardId}
+            onChange={(event) => {
+              setCardId(event.target.value)
+            }}
+          />
+        </div>
+        <Link to='/' >
+          <Button
+            text='Create New Card'
+            callback={() => {
+              // ADD CARD TO BACKEND
+              Api.serverApi.cards.create({ imdb_id: cardId })
+              console.log('new card, what do you think?')
+            }}
+          />
+        </Link>
+      </div>
+    </>
   )
 }
