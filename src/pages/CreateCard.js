ae72c12b99db56d46175f3fb55c167daaf425626
Ellezique <gizellevanzyl@gmail.com>
2021-08-11 14:29:48 +1000

    Make contact page responsive

--- src/pages/CreateCard.js
+++ src/pages/CreateCard.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Button from './../components/Button'
 import Api from './../utils/Api'
 import { Link } from 'react-router-dom'
+import './CreateCard.css'
 
 export default function CreateCard() {
   const [cardId, setCardId] = useState('')
@@ -9,17 +10,19 @@ export default function CreateCard() {
   return (
     <>
       <video src='https://res.cloudinary.com/ellezique/video/upload/v1628311932/smokeloop_oubxna.mp4' autoPlay loop muted />
-      <div className='gencontainer'>
+      <div className='createcontainer'>
         <h1>New Movie/Series Card</h1>
         <h3>Go to <a href='https://www.imdb.com/' className="hyperlink" target="_blank" rel="noreferrer">Internet Movie Database</a> and search for the movie/series you are looking for using IMDB's search tool shown below:</h3>
-        <a href='https://www.imdb.com/' target="_blank" rel="noreferrer">
-          <img src='./../../images/imdbsearch.PNG' alt='IMDB Page'/>
-        </a>
-        <br></br>
         <div className="imdbcontainer">
-          <h3>When on your chosen movie/series page, look at the url at the top of the browser and extract the id (Starts with tt) from the url.</h3>
           <a href='https://www.imdb.com/' target="_blank" rel="noreferrer">
-            <img src='./../../images/ttid.PNG' alt='Url Link Showing id'/>
+            <img className="imdbimage" src='./../../images/imdbsearch.PNG' alt='IMDB Page'/>
+          </a>
+        </div>
+        <br></br>
+        <h3>When on your chosen movie/series page, look at the url at the top of the browser and extract the id (Starts with tt) from the url.</h3>
+        <div className="imdbcontainer">
+          <a href='https://www.imdb.com/' target="_blank" rel="noreferrer">
+            <img className="imdbimage" src='./../../images/ttid.PNG' alt='Url Link Showing id'/>
           </a>
         </div>
         <br></br>
